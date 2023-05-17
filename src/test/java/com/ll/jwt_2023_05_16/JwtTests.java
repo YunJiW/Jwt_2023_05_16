a4fe8a458d7360d684d4cad83b023cffb6ce7cb0
YunJiW <dtroy371@gmail.com>
2023-05-17 09:16:24 +0900

    시크릿 키는 ì¡존재해야한다.

---
+++ src/test/java/com/ll/jwt_2023_05_16/JwtTests.java
@@ -0,0 +1,20 @@
+package com.ll.jwt_2023_05_16;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest
+class JwtTests {
+	@Value("${custom.jwt.secretKey}")
+	private String secretKey;
+
+	@Test
+	@DisplayName("secrectKey는 존재해야합니다.")
+	void t1() {
+		assertThat(secretKey).isNotNull();
+	}
+
+}
