0b1aae345ece9abf1181a252cda525a73f370dca
YunJiW <dtroy371@gmail.com>
2023-05-17 09:31:39 +0900

    시크릿키 객체

--- src/test/java/com/ll/jwt_2023_05_16/JwtTests.java
+++ src/test/java/com/ll/jwt_2023_05_16/JwtTests.java
@@ -1,20 +1,36 @@
 package com.ll.jwt_2023_05_16;
 
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
 import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class JwtTests {
 	@Value("${custom.jwt.secretKey}")
-	private String secretKey;
+	private String secretKeyPlain;
 
 	@Test
 	@DisplayName("secrectKey는 존재해야합니다.")
 	void t1() {
+		assertThat(secretKeyPlain).isNotNull();
+	}
+
+	@Test
+	@DisplayName("SecrectKey 원문으로 hmac 암호화 알고리즘에 맞는 SecrectKey 객체를 만들 수 있다.")
+	void t2(){
+		String keyBase64Encoded =
+				Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
+
+		SecretKey secretKey = Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
+
 		assertThat(secretKey).isNotNull();
+
 	}
 
 }
